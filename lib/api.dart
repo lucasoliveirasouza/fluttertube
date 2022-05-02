8478fde3bea3b1dd1e6271c4d8f765e1eef8119f
lucassouzapb <lycasoliveira@gmail.com>
2022-05-02 17:01:36 -0300

    conectar com api do google #1

---
+++ lib/api.dart
@@ -0,0 +1,9 @@
+import 'package:http/http.dart' as http;
+
+const API_KEY = "AIzaSyDcMaGK3npChAIeIUvx5K1ht7QWxritMoQ";
+
+class Api {
+  search(String search) async {
+    http.Response response = await http.get();
+  }
+}
