d3727c4dc3124a444a37b8df54aca3ad50f6095f
lucassouzapb <lycasoliveira@gmail.com>
2022-05-02 17:20:35 -0300

    conectar com api do google #1

--- lib/api.dart
+++ lib/api.dart
@@ -1,9 +1,33 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 const API_KEY = "AIzaSyDcMaGK3npChAIeIUvx5K1ht7QWxritMoQ";
 
 class Api {
+  final headers = <String, String>{};
+
   search(String search) async {
-    http.Response response = await http.get();
+    http.Response response = await attemptRequest1(
+        Uri.parse(
+            "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"),
+        http.get,
+        headers: headers);
+
+    decode(response);
   }
+
+  decode(http.Response response) {
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+    }
+  }
+}
+
+Future<http.Response> attemptRequest1(
+    Uri uri,
+    Future<http.Response> Function(Uri, {Map<String, String>? headers})
+        requestFunction1,
+    {Map<String, String>? headers}) async {
+  return await requestFunction1(uri, headers: headers);
 }
